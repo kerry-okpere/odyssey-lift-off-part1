dbef02dd0fcc1eaa798af88ec6101b4e6a943fe4
kerry-okpere <kerryokpere@gmail.com>
2023-02-06 00:09:43 +0100

    lift 1 complete

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+
+const TRACKS = gql`
+  query TracksForHome {
+    tracksForHome {
+      author {
+        id
+        name
+        photo
+      }
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO kerry
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const mocks = require("./mock");
+
+const server = new ApolloServer({typeDefs, mocks})
+
+server.listen().then(() => {
+  console.log(`
+    🚀  Server is running!
+    🔉  Listening on port 4000
+    📭  Query at http://localhost:4000
+  `);
+})
